4e39da4767f5bfc8377ab67ca762b478b8d49736
Estifanos <estifanosbereket297@gmail.com>
2025-10-24 10:45:39 +0300

    feat: refactored into small functions

--- internal/command/fix.go
+++ internal/command/fix.go
@@ -14,6 +14,100 @@ import (
 //go:embed gradle.properties
 var gradleTemplate []byte
 
+func isReactnativeApp(files *[]os.DirEntry) bool {
+	var isNative bool
+	for _, val := range *files {
+		if val.Name() == "app.json" {
+			isNative = true
+			break
+		}
+	}
+	return isNative
+}
+func runPreBuild(cmd *exec.Cmd) error {
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Stdin = os.Stdin
+
+	if err := cmd.Run(); err != nil {
+		fmt.Println("Error:", err)
+		return err
+	}
+	return nil
+}
+
+func findAndroidFolder(afterBuild *[]os.DirEntry) bool {
+	var foundAndroid bool
+	for _, val := range *afterBuild {
+		if val.Name() == "android" {
+			foundAndroid = true
+			break
+		}
+	}
+	return foundAndroid
+}
+
+func updateJavaVerions(cmdJava *exec.Cmd) error {
+	cmdJava.Stdout = os.Stdout
+	cmdJava.Stderr = os.Stderr
+	cmdJava.Stdin = os.Stdin
+
+	if err := cmdJava.Run(); err != nil {
+		fmt.Println("Error:", err)
+		return nil
+	}
+	return nil
+}
+
+func updateJavacVerions(cmdJavaC *exec.Cmd) error {
+	cmdJavaC.Stdout = os.Stdout
+	cmdJavaC.Stderr = os.Stderr
+	cmdJavaC.Stdin = os.Stdin
+
+	if err := cmdJavaC.Run(); err != nil {
+		fmt.Println("Error:", err)
+		return nil
+	}
+	return nil
+}
+
+func changeAndGetDir(dirName string) ([]os.DirEntry, string, error) {
+	androidDirErr := os.Chdir(dirName)
+	if androidDirErr != nil {
+		return nil, "", androidDirErr
+	}
+	androidDir, errAndroidDir := os.Getwd()
+	if errAndroidDir != nil {
+		return nil, "", errAndroidDir
+	}
+	androidDirFiles, androidDirFilesErr := os.ReadDir(androidDir)
+
+	if androidDirFilesErr != nil {
+		return nil, "", androidDirFilesErr
+	}
+	return androidDirFiles, androidDir, nil
+}
+
+func hasGradleFile(androidDirFiles *[]os.DirEntry, fileName string) bool {
+	var hasGradleFile bool
+
+	for _, androidF := range *androidDirFiles {
+		if androidF.Name() == fileName {
+			hasGradleFile = true
+			break
+		}
+	}
+	return hasGradleFile
+}
+
+func writeGradleFile(fullPath string) error {
+	err := os.WriteFile(fullPath, gradleTemplate, 0644)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
 var fixCmd = &cobra.Command{
 	Use:   "fix",
 	Short: "Commad to generate native folders and fix file",
@@ -21,122 +115,86 @@ var fixCmd = &cobra.Command{
 	RunE: func(cmd *cobra.Command, args []string) error {
 		dir, err := os.Getwd()
 		if err != nil {
-			panic(err)
+			return err
 		}
 		fmt.Println("Current working directory:", dir)
-		files, err := os.ReadDir(dir)
-		var isRnApp bool
+		files, errFile := os.ReadDir(dir)
 
-		for _, val := range files {
-			if val.Name() == "app.json" {
-				isRnApp = true
-				break
-			}
-			isRnApp = false
+		if errFile != nil {
+			return errFile
 		}
+		isRnApp := isReactnativeApp(&files)
+
 		if isRnApp {
 			fmt.Println("The App is react native app ")
 			cmd := exec.Command("npx", "expo", "prebuild", "--platform", "android")
 
-			cmd.Stdout = os.Stdout
-			cmd.Stderr = os.Stderr
-			cmd.Stdin = os.Stdin
+			preBildErr := runPreBuild(cmd)
 
-			if err := cmd.Run(); err != nil {
-				fmt.Println("Error:", err)
-				return nil
+			if preBildErr != nil {
+				return preBildErr
 			}
 
 			fmt.Println("Expo prebuild completed successfully!")
 
-			afterBuild, err := os.ReadDir(dir)
+			afterBuild, afterBuildErr := os.ReadDir(dir)
 
-			if err != nil {
-				fmt.Println(err)
-				panic(err)
+			if afterBuildErr != nil {
+				fmt.Println(afterBuildErr)
+				return afterBuildErr
 			}
 
-			foundAndroid := false
-
-			for _, val := range afterBuild {
-				if val.Name() == "android" {
-					foundAndroid = true
-					break
-				}
-			}
+			foundAndroid := findAndroidFolder(&afterBuild)
 
 			if foundAndroid {
 				fmt.Println("We Found the android folder")
 			} else {
 				fmt.Println("We didnt find the android folder")
-				return nil
+				return fmt.Errorf("couldn't get android folder")
 			}
 
 			cmdJava := exec.Command("sudo", "update-alternatives", "--config", "java")
 
-			cmd.Stdout = os.Stdout
-			cmd.Stderr = os.Stderr
-			cmd.Stdin = os.Stdin
+			cmdJavaError := updateJavaVerions(cmdJava)
 
-			if err := cmdJava.Run(); err != nil {
-				fmt.Println("Error:", err)
-				return nil
+			if cmdJavaError != nil {
+				fmt.Println(cmdJavaError)
+				panic(cmdJavaError)
 			}
 
 			cmdJavac := exec.Command("sudo", "update-alternatives", "--config", "javac")
 
-			cmd.Stdout = os.Stdout
-			cmd.Stderr = os.Stderr
-			cmd.Stdin = os.Stdin
+			cmdJavaCErr := updateJavacVerions(cmdJavac)
 
-			if err := cmdJavac.Run(); err != nil {
-				fmt.Println("Error:", err)
-				return nil
+			if cmdJavaCErr != nil {
+				fmt.Println(cmdJavaCErr)
+				panic(cmdJavaCErr)
 			}
 
-			androidDirErr := os.Chdir("android")
+			androidDirFiles, androidDir, getAndroidDir := changeAndGetDir("android")
 
-			if androidDirErr != nil {
-				panic(androidDirErr)
+			if getAndroidDir != nil {
+				fmt.Println(getAndroidDir)
+				return getAndroidDir
 			}
 
-			androidDir, errAndroidDir := os.Getwd()
-			if errAndroidDir != nil {
-				panic(errAndroidDir)
-			}
-
-			androidDirFiles, androidDirFilesErr := os.ReadDir(androidDir)
-
-			if androidDirFilesErr != nil {
-				panic(androidDirFilesErr)
-			}
-			hasGradleFile := false
-			for _, androidF := range androidDirFiles {
-				fmt.Println(androidF.Name())
-				if androidF.Name() == "gradle.properties" {
-					hasGradleFile = true
-					break
-				}
-			}
+			hasGradleFile := hasGradleFile(&androidDirFiles, "gradle.properties")
 			if hasGradleFile {
-
 				fullPath := filepath.Join(androidDir, "gradle.properties")
-				err := os.WriteFile(fullPath, gradleTemplate, 0644)
+				err := writeGradleFile(fullPath)
 				if err != nil {
-					panic(err)
+					return err
 				}
-				fmt.Println("Writing the gradle file completed")
+				fmt.Println("Gradle File written succesfully")
 			}
-
 		} else {
 			fmt.Println("Not a react native app")
+			return fmt.Errorf("this is not a react native project")
 		}
 
 		return nil
 	},
 }
-
-var pwd string
 
 func init() {
 	rootCmd.AddCommand(fixCmd)
